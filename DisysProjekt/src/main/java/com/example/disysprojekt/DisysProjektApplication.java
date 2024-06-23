5d3db9bdca4e2f2342d52cf605545390f468e820
MiloooMilo <131277273+MiloooMilo@users.noreply.github.com>
2024-06-23 23:44:39 +0200

    initial commit

---
+++ DisysProjekt/src/main/java/com/example/disysprojekt/DisysProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.disysprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisysProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisysProjektApplication.class, args);
+	}
+
+}
---
+++ DisysProjekt/src/test/java/com/example/disysprojekt/DisysProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.disysprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisysProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
