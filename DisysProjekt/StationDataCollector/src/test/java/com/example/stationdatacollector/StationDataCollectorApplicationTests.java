29d7f4fd3be4f5b8bc865bb07e548f6f197b3805
Milorad <wi22b002@technikum-wien.at>
2024-06-24 15:37:49 +0200

    StationDataCollectorApplicationTests UnitTest

--- DisysProjekt/StationDataCollector/src/test/java/com/example/stationdatacollector/StationDataCollectorApplicationTests.java
+++ DisysProjekt/StationDataCollector/src/test/java/com/example/stationdatacollector/StationDataCollectorApplicationTests.java
@@ -1,13 +1,53 @@
 package com.example.stationdatacollector;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.mockito.Mockito;
+
+import java.util.List;
 
 @SpringBootTest
-class StationDataCollectorApplicationTests {
+@AutoConfigureMockMvc
+public class StationDataCollectorApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private DataCollectionService dataCollectionService;
+
+	@MockBean
+	private StationDataCollectorController controller;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		// This test will pass if the application context loads successfully
 	}
 
+	@Test
+	public void testProcessStationData() throws Exception {
+		// Mock the service response
+		Mockito.when(dataCollectionService.collectDataFromStationDb(Mockito.anyString(), Mockito.anyString()))
+				.thenReturn(List.of("data1", "data2"));
+
+		// Test the processStationData method
+		String message = "1;http://example.com/stationDb";
+		controller.processStationData(message);
+
+		// Verify that the data was processed correctly
+		Mockito.verify(controller).processStationData(message);
+	}
+
+	@Test
+	public void testRun() throws Exception {
+		// Test the run method
+		controller.run("testQueue", "testBrokerUrl");
+
+		// Verify that the controller started listening for messages
+		Mockito.verify(controller).run("testQueue", "testBrokerUrl");
+	}
 }
